7c6cfb9f5fa2c01cc2d67968ce4f5768c1b8443b
HizRadit07 <hizkiagenius@gmail.com>
2021-04-16 16:53:48 +0700

    added function to read from an array of strings

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, request
-
+import re
 
 app = Flask(__name__)
 
+arr = ["hi","there","whats up maggots"]
+
+def convertArrToString(array):
+    s = ""
+    for elements in array:
+        s += elements+"<br/>"; #newline buat html pake <br/>
+    return s
+
+myString = convertArrToString(arr);
+
+print(myString)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg') #INI YG BAKAL DIPROSES
+    return myString
 
 
 if __name__ == "__main__":
